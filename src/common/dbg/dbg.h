184d36c867898213d80325e61ed5db8971468a0a
Lara Maia <dev@lara.click>
2017-05-26 11:16:28 -0300

    Update headers

--- src/common/dbg/dbg.h
+++ src/common/dbg/dbg.h
@@ -1,17 +1,21 @@
-/**
+/*
+ * IPFUnpacker is a free tool for extract and decrypt ipf files
  *
- *   ██████╗   ██╗ ███████╗ ███╗   ███╗ ██╗   ██╗
- *   ██╔══██╗ ███║ ██╔════╝ ████╗ ████║ ██║   ██║
- *   ██████╔╝ ╚██║ █████╗   ██╔████╔██║ ██║   ██║
- *   ██╔══██╗  ██║ ██╔══╝   ██║╚██╔╝██║ ██║   ██║
- *   ██║  ██║  ██║ ███████╗ ██║ ╚═╝ ██║ ╚██████╔╝
- *   ╚═╝  ╚═╝  ╚═╝ ╚══════╝ ╚═╝     ╚═╝  ╚═════╝
+ * Spl3en <spl3en.contact@gmail.com> 2015 ~ 2016
+ * Lara Maia <dev@lara.click> 2017
  *
- * @file dbg.h
- * @brief Provides debug and dump functions
+ * IPFUnpacker is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * Provides debug functions with multiple levels and dump utilities.
+ * IPFUnpacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with IPFUnpacker.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
--- src/ipf_unpack/ipf.c
+++ src/ipf_unpack/ipf.c
@@ -1,14 +1,21 @@
-/**
+/*
+ * IPFUnpacker is a free tool for extract and decrypt ipf files
  *
- *   ██████╗   ██╗ ███████╗ ███╗   ███╗ ██╗   ██╗
- *   ██╔══██╗ ███║ ██╔════╝ ████╗ ████║ ██║   ██║
- *   ██████╔╝ ╚██║ █████╗   ██╔████╔██║ ██║   ██║
- *   ██╔══██╗  ██║ ██╔══╝   ██║╚██╔╝██║ ██║   ██║
- *   ██║  ██║  ██║ ███████╗ ██║ ╚═╝ ██║ ╚██████╔╝
- *   ╚═╝  ╚═╝  ╚═╝ ╚══════╝ ╚═╝     ╚═╝  ╚═════╝
+ * Spl3en <spl3en.contact@gmail.com> 2015 ~ 2016
+ * Lara Maia <dev@lara.click> 2017
  *
- * @license GNU GENERAL PUBLIC LICENSE - Version 2, June 1991
- *          See LICENSE file for further information
+ * IPFUnpacker is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * IPFUnpacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IPFUnpacker.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "ipf.h"
